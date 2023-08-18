77ab0f5b2581e3efc77653ac4f9ac4c167e7440e
matinmoradi80 <62210384+matinmoradi80@users.noreply.github.com>
2023-08-18 23:24:07 +0330

    Update JavaCup.java

--- src/JavaCup.java
+++ src/JavaCup.java
@@ -23,7 +23,7 @@ public class JavaCup {
         else { System.out.println("NO"); }
     }
     public static void temp() {
-        ArrayList a = new ArrayList();
+        ArrayList a = new ArrayList(10000*20000);
         for (int i = 0; i < 10000; i++)
         {
             for (int j = 0; j < 20000; j++) {
